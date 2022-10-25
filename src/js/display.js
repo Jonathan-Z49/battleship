94c9ed0c3d8ed495b15b4266c9f24b9d4a847d0d
Jonathan Zarate <g7xvirus49@gmail.com>
2022-10-25 02:58:34 -0500

    Moved drag and drop functions to its own file.

--- src/js/display.js
+++ src/js/display.js
@@ -1,24 +1,26 @@
-export function createBoardDOM(Gameboard) {
+import * as dragdrop from './dragdrop';
+
+function createBoardDOM(Gameboard) {
   const boardDOM = document.querySelector('#board');
-  boardDOM.addEventListener('dragleave', dragLeave);
+  boardDOM.addEventListener('dragleave', dragdrop.dragLeave);
   Gameboard.board.forEach((element) => {
     const gridCell = document.createElement('div');
     gridCell.classList.add('cell');
     gridCell.setAttribute('data-x', element.posX);
     gridCell.setAttribute('data-y', element.posY);
-    gridCell.addEventListener('dragenter', dragEnter);
-    gridCell.addEventListener('dragover', dragOver);
-    gridCell.addEventListener('drop', dragDrop);
+    gridCell.addEventListener('dragenter', dragdrop.dragEnter);
+    gridCell.addEventListener('dragover', dragdrop.dragOver);
+    gridCell.addEventListener('drop', dragdrop.dragDrop);
     boardDOM.appendChild(gridCell);
   });
 }
 
-export function createShipGridCells() {
+function createShipGridCells() {
   const shipList = document.querySelectorAll('.ship-container');
 
   shipList.forEach((shipContainer) => {
-    shipContainer.addEventListener('dragstart', dragStart);
-    shipContainer.addEventListener('dragend', dragEnd);
+    shipContainer.addEventListener('dragstart', dragdrop.dragStart);
+    shipContainer.addEventListener('dragend', dragdrop.dragEnd);
 
     const sizeOfShip = parseInt(shipContainer.getAttribute('data-ship'), 10);
 
@@ -31,7 +33,7 @@ export function createShipGridCells() {
   });
 }
 
-export function setUpAxisButton() {
+function setUpAxisButton() {
   const axisButton = document.querySelector('#axis-btn');
   const ships = document.querySelectorAll('.ship-container');
   const allShips = document.querySelector('.all-ships');
@@ -56,132 +58,9 @@ export function setUpAxisButton() {
     }
   });
 }
-function dragStart(e) {
-  e.dataTransfer.setData('text', e.target.getAttribute('data-ship'));
-  e.dataTransfer.effectAllowed = 'move';
-  setTimeout(() => {
-    e.target.classList.add('dragging');
-  }, 0);
-}
 
-function dragEnd(e) {
-  e.preventDefault();
-  if (!e.target.classList.contains('.ship-cell')) {
-    e.target.classList.remove('dragging');
-    const hoveredCells = document.querySelectorAll('.dragHover');
-    hoveredCells.forEach((cell) => {
-      cell.classList.remove('dragHover');
-    });
-  }
-}
-
-function dragOver(e) {
-  e.preventDefault();
-}
-
-function dragEnter(e) {
-  e.preventDefault();
-  const hoveredCells = document.querySelectorAll('.dragHover');
-  const cellsWithBlockedImage = document.querySelectorAll('.blocked');
-  cellsWithBlockedImage.forEach((cell) => {
-    cell.classList.remove('blocked');
-  });
-
-  hoveredCells.forEach((cell) => {
-    cell.classList.remove('dragHover');
-  });
-
-  const draggedCell = document.querySelector('.dragging');
-  const shipSize = parseInt(draggedCell.getAttribute('data-ship'), 10);
-
-  const coords = {
-    x: e.target.getAttribute('data-x'),
-    y: e.target.getAttribute('data-y'),
-  };
-
-  const axisButton = document.querySelector('#axis-btn');
-  const axisToUse = axisButton.getAttribute('data-axis');
-
-  let oppositeAxis;
-  if (axisToUse === 'x') {
-    oppositeAxis = 'y';
-  } else {
-    oppositeAxis = 'x';
-  }
-
-  const cellsInDirection = document.querySelectorAll(`[data-${oppositeAxis}='${coords[oppositeAxis]}']`);
-
-  const cellsAfterCurrent = 9 - coords[axisToUse];
-  const cellsBeforeCurrent = coords[axisToUse] - 1;
-  const cellsToChange = [];
-
-  if (cellsAfterCurrent >= shipSize - 1) {
-    for (let index = 0; index < cellsInDirection.length; index++) {
-      if (parseInt(cellsInDirection[index].getAttribute(`data-${axisToUse}`), 10) > coords[axisToUse]) {
-        cellsToChange.push(cellsInDirection[index]);
-      }
-    }
-  } else if (cellsBeforeCurrent >= shipSize - 1) {
-    for (let index = 0; index < cellsInDirection.length; index++) {
-      if (parseInt(cellsInDirection[index].getAttribute(`data-${axisToUse}`), 10) < coords[axisToUse]) {
-        cellsToChange.push(cellsInDirection[index]);
-      }
-    }
-    cellsToChange.reverse();
-  }
-
-  cellsToChange.splice(0 + shipSize - 1);
-
-  let isCellUsed = false;
-  let cellsToRemoveHover;
-
-  e.target.classList.add('dragHover');
-
-  for (let i = 0; i < cellsToChange.length; i++) {
-    if (cellsToChange[i].classList.contains('used')) {
-      isCellUsed = true;
-      cellsToRemoveHover = document.querySelectorAll('.dragHover');
-      break;
-    } else {
-      cellsToChange[i].classList.add('dragHover');
-    }
-  }
-
-  if (isCellUsed) {
-    cellsToRemoveHover.forEach((cell) => {
-      cell.classList.remove('dragHover');
-    });
-    e.target.classList.add('blocked');
-  }
-}
-
-function dragDrop(e) {
-  e.preventDefault();
-  const blockedCell = document.querySelector('.blocked');
-  console.log(blockedCell);
-  if (blockedCell) {
-    console.log('drop');
-    blockedCell.classList.remove('blocked');
-  }
-  const shipID = e.dataTransfer.getData('text');
-  const shipDOMElement = document.querySelector(`[data-ship='${shipID}']`);
-  const hoveredCells = document.querySelectorAll('.dragHover');
-  const { childNodes } = shipDOMElement;
-
-  for (let i = 0; i < hoveredCells.length; i++) {
-    hoveredCells[i].appendChild(childNodes[i].cloneNode(true));
-    hoveredCells[i].classList.add('used');
-    hoveredCells[i].removeEventListener('dragenter', dragEnter, false);
-    hoveredCells[i].removeEventListener('dragover', dragOver, false);
-    hoveredCells[i].removeEventListener('drop', dragDrop, false);
-  }
-}
-
-function dragLeave(e) {
-  if (e.target.id === 'board') {
-    const hoveredCells = document.querySelectorAll('.dragHover');
-    hoveredCells.forEach((cell) => {
-      cell.classList.remove('dragHover');
-    });
-  }
+export default function initialize(Gameboard) {
+  createBoardDOM(Gameboard);
+  createShipGridCells();
+  setUpAxisButton();
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,8 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'modern-normalize/modern-normalize.css';
 import '../styles.scss';
-import * as displayDOM from './display';
+import initialize from './display';
 import GameBoard from './gameboard';
 
 const myBoard = new GameBoard();
-displayDOM.createBoardDOM(myBoard);
-displayDOM.createShipGridCells();
-displayDOM.setUpAxisButton();
+initialize(myBoard);
